96dc7775aab6c44670d32ec89928ea946690a2ce
Bloody_Snow <Georgii200321@gmail.com>
2024-02-08 00:36:40 +0700

    first commit

---
+++ Lab_1/Расчёт графов.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import random
+import time
+
+# Функция по созданию графа
+def create_graph(nodes, edges):
+    G = nx.Graph()
+    for i in range(nodes):
+        G.add_node(i)
+    while len(G.edges()) < edges:
+        node1 = random.randint(0, nodes-1)
+        node2 = random.randint(0, nodes-1)
+        if node1 != node2 and not G.has_edge(node1, node2):
+            weight = random.randint(1, 10)
+            G.add_edge(node1, node2, weight=weight)
+    return G
+
+# Функция по расчёту кратчайшего метода
+def calculate_shortest_path(G, method):
+    start_time = time.time()
+    if method == 'Дейкстры':
+        shortest_paths = nx.all_pairs_dijkstra_path(G)
+    elif method == 'Беллмана-Форда':
+        shortest_paths = nx.all_pairs_bellman_ford_path(G)
+    elif method == 'Флойда-Уоршелла':
+        shortest_paths = nx.floyd_warshall(G)
+    else:
+        print("Неизвестный метод")
+        return
+    end_time = time.time()
+    print(f"Время используемое алгоритмом {method}: {end_time - start_time} seconds")
+    return shortest_paths
+
+graphs = int(input("Введите количество графов: "))
+nodes = int(input("Введите количество узлов: "))
+edges = int(input("Введите количество рёбер: "))
+
+for i in range(graphs):
+    G = create_graph(nodes, edges)
+    print(f"Graph {i+1}:")
+    for method in ['Дейкстры', 'Беллмана-Форда', 'Флойда-Уоршелла']:
+        shortest_paths = calculate_shortest_path(G, method)
+        print(f"Кратчайшие пути рассчитываются с помощью алгоритма {method}: {shortest_paths}")
